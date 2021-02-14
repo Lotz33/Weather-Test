0adbd6ab8a31093a48ba6a9d64a710e1a12611b1
Lotz33 <55788592+Lotz33@users.noreply.github.com>
2021-02-14 15:29:11 +0000

    Forecast created

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 
 function changeDate() {
   let now = new Date();
-  let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+  console.log(now);
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let day = days[now.getDay()];
+  console.log(day);
   let h = now.getHours();
   if (h < 10) {
     h = `0${h}`;
@@ -22,11 +24,44 @@ function changeDate() {
 changeDate();
 
 ///////////////////////// SEARCH
+function formatHours(timestamp) {
+  let now = new Date(timestamp);
+  let h = now.getHours();
+  if (h < 10) {
+    h = `0${h}`;
+  }
+  let m = now.getMinutes();
+  if (m < 10) {
+    m = `0${m}`;
+  }
 
+  return `${h}:${m}`;
+}
+
+function showForecast(response) {
+  // console.log(response.data);
+  let forecast = null;
+  document.querySelector("#forecast").innerHTML = null;
+
+  for (let i = 0; i <= 5; i++) {
+    forecast = response.data.list[i];
+
+    document.querySelector("#forecast").innerHTML += `
+ 
+    <td>${formatHours(forecast.dt * 1000)}</td>
+    <td >${Math.round(forecast.main.temp_max)}°C | ${Math.round(forecast.main.temp_min)}°C</td>
+    <td><img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="" id="icon2" class="float-right"/></td>
+  
+  `;
+  }
+}
 function search(city) {
   let apiKey = "5a7957f3b705a03d6f7c072ce3f964e0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?`;
   axios.get(`${apiUrl}q=${city}&units=metric&appid=${apiKey}`).then(showTemp);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?`;
+  axios.get(`${apiUrl}q=${city}&units=metric&appid=${apiKey}`).then(showForecast);
 }
 
 function showTemp(response) {
@@ -44,8 +79,16 @@ function showTemp(response) {
   document.querySelector("#description").innerHTML = response.data.weather[0].description;
   document.querySelector("#high").innerHTML = Math.round(response.data.main.temp_max);
   document.querySelector("#low").innerHTML = Math.round(response.data.main.temp_min);
-  document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  document.querySelector("#icon").setAttribute("alt", response.data.weather[0].description);
+  document.querySelector("#icon1").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#icon1").setAttribute("alt", response.data.weather[0].description);
+
+  let weather = response.data.weather[0].main;
+
+  if (weather === "Rain") {
+    document.querySelector("#rain-response").innerHTML = `<strong>Yes</strong>, take a brolly! <i class="fas fa-umbrella"></i>`;
+  } else {
+    document.querySelector("#rain-response").innerHTML = `<strong>Not at the moment</strong>, but check the forecast.</i>`;
+  }
 }
 
 function input(event) {
@@ -104,3 +147,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 search("London, uk");
+
+///////////////////////// IS IT RAINING?
